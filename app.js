37ae1a033ffeddda857d90cc9973a0a6bfc9ab84
sam_arbid <samarbid13@users.noreply.github.com>
2019-11-20 16:51:17 +0100

    Conditionals

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
 new Vue({
   el: "#App",
+
   data: {
-    name: "Sam",
-    age: 35,
-    a: 0,
-    b: 0
+    error: false,
+    success: false
   },
 
-  methods: {
-    logName: function() {
-      console.log(this.name);
-    },
-    logAge: function() {
-      console.log(this.age);
-    },
+  methods: {},
 
-    /*addA: function() {
-      return this.a + this.age;
-    },
-    addB: function() {
-      return this.b + this.age;
-    }
-  }, */
-  },
-
-  computed: {
-    addA: function() {
-      console.log("A");
-      
-      return this.a + this.age;
-    },
-    addB: function() {
-      console.log("B");
-      
-      return this.b + this.age;
-    }
-  }
+  computed: {}
 });
 
 
