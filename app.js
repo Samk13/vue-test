c6578beaffa8932918fa3552f3645ca050fc23b0
Sam Arbid <samarbid13@gmail.com>
2019-11-23 21:39:13 +0100

    refs

--- app.js
+++ app.js
@@ -1,55 +1,17 @@
-Vue.component('greeting', {
-  template: `<p>Hey I am {{ name }} created with vue.component
-                <button @click="changeName">change name </button>
+let app = new Vue({
+  el:"#App",
+  data: {
+    output: "your fav food ! 🥦"
 
-              </p>`,
-  data: function(){
-    return {
-      name : 'Sam'
-    }
   },
-  methods : {
-    changeName : function(){
-      this.name = '🤑🤑🤑🤑🤑'
+  methods:{
+    readRefs: function(){
+      console.log(this.$refs);
+      
+      console.log(this.$refs.input.value);
+      this.output = this.$refs.input.value
+      console.log(this.$refs.test.innerText);
+      
     }
   }
 })
-
-let one = new Vue({
-  el: "#vue-app-one",
-
-  data: {
-    title: 'Vue app one !😀' 
-  },
-
-  methods: {
-    
-  },
-  computed: {
-    greet : function(){
-      return `Hello from instance 1`
-    }
-  }
-});
-
-// ----------------------------
-
-let two = new Vue({
-  el: "#vue-app-tow",
-
-  data: {
-    title: 'Vue app tow !😀😋✌🏼✌🏼'
-  },
-
-  methods: {
-    changeTitle: function(){
-     one.title = `now I changed the title 👻` 
-    }
-  },
-  computed: {
-    greet : function(){
-      return `Hello from instance 2  🤠 `
-    },
-    
-  }
-});
